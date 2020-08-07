8178d14b96c59ca6ba587fa901312375c4e1cf30
rodrigo-sntg <digo_santiago@hotmail.com>
2020-08-07 19:39:01 -0300

    nlw

---
+++ scripts/addField.js
@@ -0,0 +1,9 @@
+document.querySelector('#add-time')
+
+.addEventListener('click', clonoField)
+
+function clonoField() {
+    const fields = document.querySelector('.schedule-item').cloneNode(true)
+
+    document.querySelector('#schedule-items').appendChild(fields)
+}
